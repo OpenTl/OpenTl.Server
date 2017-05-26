375d724f3311878e467d95330a0c498748ee75e3
Виктор Борисов <viktor.borisov@bars-open.ru>
2017-05-26 22:43:08 +0700

    was init a front server and intergration tests projects

---
+++ src/OpenTl.Server.Front/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+
+namespace OpenTl.Server.Front
+{
+    class Program
+    {
+        static void Main() => RunServerAsync().Wait();
+
+        static async Task RunServerAsync()
+        {
+                var bossGroup = new MultithreadEventLoopGroup(1);
+            var workerGroup = new MultithreadEventLoopGroup();
+
+            try
+            {
+                var bootstrap = new ServerBootstrap();
+                bootstrap
+                    .Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .Option(ChannelOption.SoBacklog, 100)
+                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                     
+                    }));
+
+                IChannel boundChannel = await bootstrap.BindAsync(433);
+
+                Console.ReadLine();
+
+                await boundChannel.CloseAsync();
+            }
+            finally
+            {
+                await Task.WhenAll(
+                    bossGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)),
+                    workerGroup.ShutdownGracefullyAsync(TimeSpan.FromMilliseconds(100), TimeSpan.FromSeconds(1)));
+            }
+        }
+    }
+}
